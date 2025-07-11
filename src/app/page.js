2535ed4c1beb3152c9f454d6e2305274bf85ae21
Rakesh Singh <rakesh.singh@Geminisolutions.com>
2025-07-11 19:22:45 +0530

    fix: page modified

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
+export const dynamic = "force-dynamic";
 import Dashboard from "./pages/index";
-import Image from "next/image";
 import styles from "./page.module.css";
-// import "fab-plugins-library/dist/fab-plugins-library.css";
 
 export default function Home() {
   return (
