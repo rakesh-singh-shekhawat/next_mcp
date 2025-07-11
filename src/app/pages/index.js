da33b1647e77638f12a54483487859684a96e146
Rakesh Singh <rakesh.singh@Geminisolutions.com>
2025-07-11 19:08:50 +0530

    added some code

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,11 +1,21 @@
-import dynamic from "next/dynamic";
-
+import { useEffect, useState } from "react";
+("use client");
 const Dashboard = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div>
-      <h1>Hello There!</h1>
+      <h1>MCP API Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 };
 
 export default Dashboard;
+// For Next.js App Router
